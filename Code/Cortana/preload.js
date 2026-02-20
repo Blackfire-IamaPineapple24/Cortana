78d49722ed2da4e3e0ba700b829f7be95264f459
Jack <jack.blackfire.interactive@gmail.com>
2026-02-20 22:10:53 +0000

    Clean up code

--- Code/Cortana/preload.js
+++ Code/Cortana/preload.js
@@ -1,17 +1,17 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  askAI: (prompt) => ipcRenderer.invoke('ask-ai', prompt),
-  getUsername: () => ipcRenderer.invoke('get-username'),
-  onThemeUpdate: (callback) => ipcRenderer.on('theme-updated', (_, isLight) => callback(isLight)),
-  getWeatherLocation: () => ipcRenderer.invoke('get-weather-location'),
-  getDateFormat: () => ipcRenderer.invoke('get-date-format'),
-  fetchWeather: async () =>
-  {
-    const location = await ipcRenderer.invoke('get-weather-location');
-    const res = await fetch(`https://wttr.in/${location}?format=j1`);
-    return await res.json();
-  },
+    askAI: (prompt) => ipcRenderer.invoke('ask-ai', prompt),
+    getUsername: () => ipcRenderer.invoke('get-username'),
+    onThemeUpdate: (callback) => ipcRenderer.on('theme-updated', (_, isLight) => callback(isLight)),
+    getWeatherLocation: () => ipcRenderer.invoke('get-weather-location'),
+    getDateFormat: () => ipcRenderer.invoke('get-date-format'),
+    fetchWeather: async () =>
+    {
+        const location = await ipcRenderer.invoke('get-weather-location');
+        const res = await fetch(`https://wttr.in/${location}?format=j1`);
+        return await res.json();
+    },
 });
 
 /* This script lets the renderer send the user's messages to main.js and take Gemma's
